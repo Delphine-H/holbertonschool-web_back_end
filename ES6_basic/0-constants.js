8d15085208127607f8137f8a62f495e4dd0bd0bb
Delphine HANNON <hannon.delphine@gmail.com>
2024-07-17 13:26:01 +0200

    correct style

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -2,15 +2,13 @@ export function taskFirst() {
     const TASK = 'I prefer const when I can.';
     return TASK;
   }
-  
+
   export function getLast() {
     return ' is okay';
   }
-  
+
   export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
-  
     return combination;
   }
-  
