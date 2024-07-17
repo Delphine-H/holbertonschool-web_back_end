43405ace980c1da36dc77dca514bbf6a7fbcf94b
Delphine HANNON <hannon.delphine@gmail.com>
2024-07-17 22:17:02 +0200

    Rewrite the function to use ES6 for...of operator

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const resultArray = [];
+  for (const value of array) {
+    resultArray.push(appendString + value);
+  }
+
+  return resultArray;
+}
+  
