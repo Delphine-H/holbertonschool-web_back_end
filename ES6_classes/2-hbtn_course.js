aeb01d03f78bc9d4938c0861834e808dbd8a0603
Delphine HANNON <hannon.delphine@gmail.com>
2024-07-19 13:33:23 +0200

    use this.

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,63 +1,49 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    // Type validation
-    HolbertonCourse._validateString(name, 'Name');
-    HolbertonCourse._validateNumber(length, 'Length');
-    HolbertonCourse._validateStringArray(students, 'Students');
-
-    // Initialize attributes
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  // Getters
+  // name
   get name() {
     return this._name;
   }
 
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  // length
   get length() {
     return this._length;
   }
 
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  // students
   get students() {
     return this._students;
   }
 
-  // Setters
-  set name(newName) {
-    HolbertonCourse._validateString(newName, 'Name');
-    this._name = newName;
-  }
-
-  set length(newLength) {
-    HolbertonCourse._validateNumber(newLength, 'Length');
-    this._length = newLength;
-  }
-
-  set students(newStudents) {
-    HolbertonCourse._validateStringArray(newStudents, 'Students');
-    this._students = newStudents;
-  }
-
-  // Static method to validate string type
-  static _validateString(value, fieldName) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${fieldName} must be a string`);
-    }
-  }
-
-  // Static method to validate number type
-  static _validateNumber(value, fieldName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${fieldName} must be a number`);
-    }
-  }
-
-  // Static method to validate array of strings
-  static _validateStringArray(value, fieldName) {
-    if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
-      throw new TypeError(`${fieldName} must be an array of strings`);
+  set students(students) {
+    if (
+      Array.isArray(students)
+      && students.every((s) => typeof s === 'string')
+    ) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
   }
 }
