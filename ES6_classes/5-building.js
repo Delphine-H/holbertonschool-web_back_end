6a26c54315e498763a9e8d72a89accb11cb75d8d
Delphine HANNON <hannon.delphine@gmail.com>
2024-07-19 15:10:52 +0200

    remove evacuation warning message

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,3 +1,5 @@
+// 5-building.js
+
 class Building {
   constructor(sqft) {
     if (
@@ -14,15 +16,6 @@ class Building {
 
   // Getter for sqft
   get sqft() {
-    return this._sqft;
-  }
-
-  // Method to be implemented by subclasses
-  evacuationWarningMessage() {
-    if (this.constructor === Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
-    // using `this` to avoid eslint error
     return this._sqft;
   }
 }
