e11145b926e0df690de161cfbae0dc7ee0ff347d
Delphine HANNON <hannon.delphine@gmail.com>
2024-07-23 13:38:28 +0200

    fix error for '' value

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,10 @@
 // Function to create a string of values from a set that start with a specific string
 function cleanSet(set, startString) {
+  // If startString is empty, return nothing
+  if (startString === '') {
+    return '';
+  }
+
   // Convert the set to an array and process each element
   return [...set]
     .filter((value) => value.startsWith(startString)) // Filter values that start with startString
