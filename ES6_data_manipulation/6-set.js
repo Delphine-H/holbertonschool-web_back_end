f3f66457468e9a3f5f5cbd438a8656c0b638ca06
Delphine HANNON <hannon.delphine@gmail.com>
2024-07-23 13:12:51 +0200

    Add function that returns a set from an array

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,7 @@
+// Function to create a Set from an array
+function setFromArray(array) {
+  return new Set(array); // Convert the array to a Set to eliminate duplicates and return the Set
+}
+
+// Export the function so it can be used in other files
+export default setFromArray;
