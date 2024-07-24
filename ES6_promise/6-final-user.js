366e3912f44d6666b95ef9b55d5b63c5bd31b595
Delphine HANNON <hannon.delphine@gmail.com>
2024-07-24 13:28:33 +0200

    refactor code

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = await signUpUser(firstName, lastName);
+  let photo = null;
+  try {
+    photo = await uploadPhoto(fileName);
+  } catch (err) {
+    photo = `${err.name}: ${err.message}`;
+  }
+  const prom1 = { status: 'fulfilled', value: user };
+  const prom2 = { status: 'rejected', value: photo };
+  return [prom1, prom2];
 }
