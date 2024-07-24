dea37064d9f9cc617a66abd95bc085cf81833fc5
Delphine HANNON <hannon.delphine@gmail.com>
2024-07-24 13:24:24 +0200

    Add function that call signUpUser and uploadPhoto and when the promises are all settled will return an array with the statue of the promise and value return.

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
+}
