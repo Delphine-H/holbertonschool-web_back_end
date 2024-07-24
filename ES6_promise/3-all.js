203dbc840f1a380932f4c561466ff20e89033a9e
Delphine HANNON <hannon.delphine@gmail.com>
2024-07-24 12:54:02 +0200

    Modification in then handler.

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,8 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const [photo, user] = results;
-      console.log(`${photo} ${user.firstName} ${user.lastName}`);
+      const { body } = results[0];
+      const { firstName } = results[1];
+      const { lastName } = results[1];
+      console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
