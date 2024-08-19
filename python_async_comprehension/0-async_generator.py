39175d1ba8955155d1c962bc5f2028b4697d6f25
Delphine HANNON <hannon.delphine@gmail.com>
2024-08-19 10:46:50 +0200

    Changed the import from `AsyncGenerator` to `Generator` to correctly reflect the type of generator being used. Updated the type annotation in the `async_generator` function to `Generator[float, None, None]

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """Coroutine that loop 10 times and each time asynchronously wait 1 second,
     then yield a random number"""
     for _ in range(10):
