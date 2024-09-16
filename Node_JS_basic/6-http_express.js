9054da82cfb56ebd0fbe00a7598fcad0437b979a
Delphine HANNON <hannon.delphine@gmail.com>
2024-09-16 16:38:11 +0200

    add a small HTTP server using Express module

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,6 +1,19 @@
+// Import the Express module
 const express = require('express');
 
+// Create an Express application
 const app = express();
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
-app.listen(1245);
+
+// Define a route for the endpoint /
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!'); // Send the response
+});
+
+// Make the application listen on port 1245
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`); // Log server start
+});
+
+// Export the application
 module.exports = app;
