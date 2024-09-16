71544a6d8edb5c18c67ad682d716f509a116dfdd
Delphine HANNON <hannon.delphine@gmail.com>
2024-09-16 12:09:28 +0200

    remove trailing newline

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,14 +2,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
   const name = input.toString().trim(); // Handle user input
-
-  // Print the name without any extra line breaks
   process.stdout.write(`Your name is: ${name}\r`);
 });
 
 // Handle EOF (End of File)
 process.stdin.on('end', () => {
   // Properly close the program
-  process.stdout.write('\nThis important software is now closing\n');
+  process.stdout.write('\nThis important software is now closing');
   process.exit(); // Cleanly terminate the program
 });
