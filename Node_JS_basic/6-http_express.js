c1d7424455fbd152828c5e8393072728342b9e96
Delphine HANNON <hannon.delphine@gmail.com>
2024-09-16 16:27:50 +0200

    add a small HTTP server using Express module

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,6 +1,29 @@
-const express = require('express');
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-const app = express();
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
-app.listen(1245);
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const data = await countStudents('database.csv');
+      res.end(`${data.join('\n')}`);
+    } catch (error) {
+      res.end('Cannot load the database\n');
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
+
 module.exports = app;
