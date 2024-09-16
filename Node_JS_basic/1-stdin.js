2ce0aef2ba42947c861479d34db00462af225860
Delphine HANNON <hannon.delphine@gmail.com>
2024-09-16 11:55:28 +0200

    fix eslint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
-    const name = input.toString().trim(); // Handle user input
-    process.stdout.write(`Your name is: ${name}\r\n`); // Ensure correct new line format
-    process.exit(); // Close the program
+  const name = input.toString().trim(); // Handle user input
+  process.stdout.write(`Your name is: ${name}\r\n`); // Ensure correct new line format
+  process.exit(); // Close the program
 });
