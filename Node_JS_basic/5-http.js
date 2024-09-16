5931999e602eb7199705c5940fb03a7513aedf89
Delphine HANNON <hannon.delphine@gmail.com>
2024-09-16 16:14:28 +0200

    add a small HTTP server using the http module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -10,17 +10,20 @@ const app = http.createServer(async (req, res) => {
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      // Ensure to pass the correct path to countStudents
-      const data = await countStudents(process.argv[2]);
-      res.end(data.join('\n'));
+      const data = await countStudents('database.csv'); // Use the correct path for the database
+      res.end(`${data.join('\n')}`);
     } catch (error) {
+      res.statusCode = 500; // Internal Server Error
       res.end(error.message);
     }
   } else {
-    res.statusCode = 404; // Return 404 for other routes
+    res.statusCode = 404; // Not Found
     res.end('Not Found');
   }
 });
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
+
 module.exports = app;
