ff440dedc8427441e8378d78bc4609096db78fd2
Delphine HANNON <hannon.delphine@gmail.com>
2024-09-16 16:24:23 +0200

    add a small HTTP server using Express module

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,20 +1,6 @@
 const express = require('express');
 
 const app = express();
-
-// Define the root route
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
-// Define a catch-all route for any endpoint not explicitly defined
-app.use((req, res) => {
-  res.status(404).send(`<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Error</title></head><body><pre>Cannot GET ${req.url}</pre></body></html>`);
-});
-
-// Start the server on port 1245
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
-
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.listen(1245);
 module.exports = app;
