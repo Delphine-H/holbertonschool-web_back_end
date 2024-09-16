37b7077553ad1b1d22cdd2c2ff6e99082eda3880
Delphine HANNON <hannon.delphine@gmail.com>
2024-09-16 11:48:20 +0200

    fix new line format

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,7 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on('data', (input) => {
-  const name = input.toString().trim(); // Get the name and remove any leading/trailing whitespace
-  process.stdout.write(`Your name is: ${name}\n`);
-
-  // Properly close the program
-  process.stdout.write('This important software is now closing\n');
-  process.exit(); // Cleanly terminate the program
+    const name = input.toString().trim(); // Handle user input
+    process.stdout.write(`Your name is: ${name}\r\n`); // Ensure correct new line format
+    process.exit(); // Close the program
 });
